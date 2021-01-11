00c621a242401b0f58c2dcd20aa723249285a3ab
Ravinder Negi <r.singh.negi85@gmail.com>
2021-01-11 12:45:08 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var hwp = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: path.join(__dirname, '/src/index.js')
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, '/out')
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.s[ac]ss$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                { loader: 'css-loader', options: { url: false, sourceMap: false } },
+                { loader: 'sass-loader', options: { sourceMap: false } }
+            ],
+        }, ]
+    },
+    //devtool: 'source-map',
+    plugins: [
+        new hwp({
+            template: path.join(__dirname, '/src/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].bundle.css"
+        })
+    ],
+    mode :'production'
+}
